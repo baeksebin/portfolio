9bdbbf2a4a76d3c55aca025a1d5bf5695bdce099
Baeksebin <bsb983@gmail.com>
2025-11-29 16:58:49 +0900

    Fix Head.js SEO logic

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   siteMetadata: {
     title: 'Baeksebin',
     description: '서버/인프라부터 코드 레벨까지',
-    siteUrl: 'https://baeksebin.github.io/portfolio', // No trailing slash allowed!
+    siteUrl: 'https://baeksebin.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@100sebim',
   },
